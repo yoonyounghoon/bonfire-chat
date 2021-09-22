4c1b1ab0244c202715a92e72e088f3d3e6837829
yoonyounghoon <yunyounghoon23@gmail.com>
2021-09-22 19:22:03 +0900

    Implements: Chat component

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,25 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { socket } from '../App';
+import Message from './Message';
 
 type ChatProps = {
   name: string;
 };
 
+type MessageType = 'notify' | 'yours' | 'me';
+
+export type MessageProps = {
+  type: MessageType;
+  text: string;
+};
+
 function Chat({ name }: ChatProps) {
+  const [messages, setMessages] = useState<MessageProps[] | []>([]);
+  const [text, setText] = useState('');
+
+  const handleNewUser = (data: any) => {
+    console.log('handle new user');
+
+    setMessages(messages.concat(data));
+  };
+
+  const handleNewMessage = (data: MessageProps) => {
+    console.log('handle new messages');
+    setMessages([...messages, data]);
+  };
+
+  const chatSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    // 채팅 전송
+    socket.emit('post message', {
+      id: socket.id,
+      text,
+    });
+
+    setText('');
+  };
+
+  const inputChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setText(e.target.value);
+  };
+
   useEffect(() => {
     socket.emit('new user', name);
-    socket.on('get newUser', (data) => {
-      console.log(data);
-    });
+    socket.on('get newUser', handleNewUser);
+    socket.on('get newMessage', handleNewMessage);
   }, []);
 
   return (
     <ChatWrapper>
       <Header>bonfire-Chat</Header>
-      <MessageList>메시지 리스트</MessageList>
-      <ChatForm>
-        <ChatInput />
+      <MessageList>
+        {messages.length > 0 &&
+          messages.map((message, idx) => (
+            <Message key={idx} type={message.type} text={message.text} />
+          ))}
+      </MessageList>
+      <ChatForm onSubmit={chatSubmit}>
+        <ChatInput
+          placeholder="무엇이든 말해도 괜찮아요"
+          value={text}
+          onChange={inputChange}
+        />
         <ChatButton>전송</ChatButton>
       </ChatForm>
     </ChatWrapper>
